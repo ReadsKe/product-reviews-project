029119cb8cacf2cefa24c3b3ea49a5a92d8b39b4
Samuel Kimote <readssamuel@.com>
2023-11-08 12:08:05 +0300

    footer

--- product-reviews/src/layout/Footer.js
+++ product-reviews/src/layout/Footer.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div class="container ">
+    <footer class="py-3 my-4">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item"><Link href="#" class="nav-link px-2 text-muted text-light">Home</Link></li>
+        <li class="nav-item"><Link href="#" class="nav-link px-2 text-muted text-light">About</Link></li>
+        <li class="nav-item"><Link href="#" class="nav-link px-2 text-muted text-light">Contact</Link></li>
+          </ul>
+        <p class="text-center .text-light">© 2022 Company, Inc</p>
+    </footer>
+    </div>
   )
 }
